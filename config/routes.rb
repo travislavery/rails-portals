73f97e56f58e6211b37ae0b46f101befeab3b9ea
TravisA <github email address>
2018-02-15 13:32:05 -0700

    add portals, quests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   resources :portals
-  devise_for :users
-  root 'portals#index'
+  resources :quests
+  resources :portals, only: [:show] do
+  	resources :quests
+  end
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
+
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
